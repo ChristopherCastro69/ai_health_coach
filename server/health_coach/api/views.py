033a16b9c73bb61e11efdb17b4b0875cbc217668
Christopher <topitops123@gmail.com>
2025-06-24 15:11:54 +0800

    [Feature][Chris] Add FoodEntryViewSet for managing food entries in the API

--- server/health_coach/api/views.py
+++ server/health_coach/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import FoodEntry
+from .serializers import FoodEntrySerializer
+
+class FoodEntryViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing food entries.
+    Provides `list`, `create`, `retrieve`, `update`, and `destroy` actions.
+    """
+    queryset = FoodEntry.objects.all().order_by('-consumed_at')
+    serializer_class = FoodEntrySerializer
